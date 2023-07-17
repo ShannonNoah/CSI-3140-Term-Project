7092f70ab1dda212feea0f594113f26f61435e1f
JeremyFang022 <jfang022@uottawa.ca>
2023-07-17 17:35:48 -0400

    wrote api endpoint functions for transactions and attendance collections

--- backend/controllers/attendanceController.js
+++ backend/controllers/attendanceController.js
@@ -4,37 +4,111 @@ const mongoose = require('mongoose');
 
 // GET all attendance records
 const getAttendanceRecords = async (req, res) => {
+    const records = await Attendance.find({}).sort({ createdAt: -1 });
 
+    return res.status(200).json(records);
 };
 
 // GET an attendance record matching an id
 const getAttendanceRecord = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const record = await Attendance.findById(id);
+
+    if (!record) {
+        return res.status(404).json({ err: 'Attendance record does not exist' });
+    }
+
+    return res.status(200).json(record);
 };
 
 // GET attendance records matching an employee id
 const getAttendanceRecordByEmployeeId = async (req, res) => {
+    const { employeeId } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(employeeId)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const records = await Attendance.find({ employeeId: employeeId });
+
+    if (!records) {
+        return res.status(404).json({ err: 'No matching attendance records' });
+    }
+
+    return res.status(200).json(records);
 };
 
 // GET attendance records matching a specific attendance type
 const getAttendanceRecordByType = async (req, res) => {
+    const { type } = req.params;
 
+    try {
+        const records = await Attendance.find({ type: type });
+
+        if (!records) {
+            return res.status(404).json({ err: 'No matching attendance records' });
+        }
+
+        return res.status(200).json(records);
+    } catch(err) {
+        console.log(err);
+
+        return res.status(400).json({ err: err.message });
+    }
 };
 
 // POST new attendance record
 const createAttendanceRecord = async (req, res) => {
+    try {
+        const record = await Attendance.create({
+            ...req.body
+        });
 
+        return res.status(200).json(transaction);
+    } catch(err) {
+        console.log(err);
+
+        return res.status(400).json({ err: err.message });
+    }
 };
 
 // DELETE an attendance record matching an id
 const deleteAttendanceRecord = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const record = await Attendance.findOneAndDelete({ _id: id });
+
+    if (!record) {
+        return res.status(404).json({ err: 'Attendance record does not exist' });
+    }
+
+    return res.status(200).json(record);
 };
 
 // UPDATE an attendance record matching a id
 const updateAttendanceRecord = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const record = await Attendance.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    }, {
+        new: true
+    });
+
+    return res.status(200).json(record);
 }
 
 module.exports = {
--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,45 +1,142 @@
 const Transaction = require('../models/Transaction.model.js');
+const Employee = require('../models/Employee.model.js');
 
 const mongoose = require('mongoose');
 
 // GET all transactions
 const getTransactions = async (req, res) => {
+    const transactions = await Transaction.find({}).sort({ createdAt: -1 });
 
+    res.status(200).json(transactions);
 };
 
 // GET a transaction matching an id
 const getTransaction = async (req, res) => {
-    
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const transaction = await Transaction.findById(id);
+
+    if (!transaction) {
+        return res.status(404).json({ err: 'Transaction does not exist' });
+    }
+
+    return res.status(200).json(transaction);
 };
 
 // GET all transactions matching an employeeId
 const getTransactionsByEmployeeId = async (req, res) => {
-    
+    const { employeeId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(employeeId)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const transactions = await Transaction.find({ employeeId: employeeId });
+
+    if (!transactions) {
+        return res.status(404).json({ err: 'No matching transactions' });
+    }
+
+    return res.status(200).json(transactions);
 };
 
 // GET all transactions matching a last name
 const getTransactionsByLastName = async (req, res) => {
-    
+    const { lastName } = req.params;
+    var res = [];
+
+    const employeeIds = await Employee.find({ lastName: lastName })
+                                .map(x => x._id.toString());
+
+    if (!employeeIds) {
+        return res.status(404).json({ err: 'No matching transactions' });
+    }
+
+    employeeIds.forEach(id => {
+        var transactions = Transaction.find({ employeeId: id });
+
+        res.concat(transactions);
+    });
+
+    return res.status(200).json(res);
 };
 
 // GET all transactions matching a first name
 const getTransactionsByFirstName = async (req, res) => {
-    
+    const { firstName } = req.params;
+    var res = [];
+
+    const employeeIds = await Employee.find({ firstName: firstName })
+                                .map(x => x._id.toString());
+
+    if (!employeeIds) {
+        return res.status(404).json({ err: 'No matching transactions' });
+    }
+
+    employeeIds.forEach(id => {
+        var transactions = Transaction.find({ employeeId: id });
+
+        res.concat(transactions);
+    });
+
+    return res.status(200).json(res);
 };
 
 // POST a new transaction
 const createTransaction = async (req, res) => {
+    try {
+        const transaction = await Transaction.create({
+            ...req.body
+        });
 
+        res.status(200).json(transaction);
+    } catch(err) {
+        console.log(err);
+
+        res.status(400).json({ err: err.message });
+    }
 };
 
 // DELETE a transaction matching an id
 const deleteTransaction = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const transaction = await Transaction.findOneAndDelete({ _id: id });
+
+    if (!transaction) {
+        return res.status(404).json({ err: 'Transaction does not exist' });
+    }
+
+    return res.status(200).json(transaction);
 };
 
 // UPDATE a transaction matching an id
 const updateTransaction = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ err: 'id is not valid ObjectId' });
+    }
+
+    const transaction = await Employee.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    }, {
+        new: true
+    });
+
+    if (!transaction) {
+        return res.status(404).json({ err: 'Transaction does not exist' });
+    }
+
+    return res.status(200).json(transaction);
 };
 
 module.exports = {
