20e446fef7b66337fdbf4d4a70658cd93481b21d
jaleelah <91485585+jaleelah@users.noreply.github.com>
2023-07-20 12:08:13 -0400

    Made create attendance record page

--- src/pages/CreateAttendanceRecord.js
+++ src/pages/CreateAttendanceRecord.js
@@ -5,6 +5,34 @@ import {Link} from "react-router-dom";
 function CreateAttendanceRecord() {
   return (<>
   <div className="color-block"></div>
-  <h1>Create Attendance Record</h1></>)
+  <h1>Create Attendance Record</h1>
+  <div class="form-box">
+      <form>
+        <div class="input-group">
+        &nbsp;
+        <div class="input-field">
+            <input type="text" placeholder="Employee ID" />
+          </div>
+          &nbsp;
+          <div class="input-field">
+            <input type="text" placeholder="Type" />
+          </div>
+        </div>
+      </form>
+      &nbsp;
+      <div className = "create-record">
+      <Link to="/CreateAttendanceRecord">
+        <button>Create Record</button>
+        </Link>
+      </div>
+
+      <div className = "go-back">
+                <Link to="/LandingPage">
+        <button>Go Back</button>
+        </Link></div>
+
+
+    </div>
+  </>)
 }
   export default CreateAttendanceRecord
