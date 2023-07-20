bdc094e4bbd77f4f3308671adff84997746cdbf8
JeremyFang022 <jfang022@uottawa.ca>
2023-07-20 19:04:13 -0400

    added cors module to middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const userRoutes = require('./routes/users');
 const employeeRoutes = require('./routes/employees');
 const correspondsToRoutes = require('./routes/correspondsTo');
@@ -12,6 +14,12 @@ const app = express();
 
 // middleware
 app.use(express.json());
+
+app.use(
+    cors({
+        origin: '*',
+    })
+);
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
